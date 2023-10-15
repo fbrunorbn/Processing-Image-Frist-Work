0323a15a9871ba6cc76194faf3f7a954c20e810b
fbrunorbn <84163782+fbrunorbn@users.noreply.github.com>
2023-10-15 09:48:11 -0300

    Add files via upload

--- Neg-log-pot-lenearPartes-Esteg.py
+++ Neg-log-pot-lenearPartes-Esteg.py
@@ -1,7 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import string
-import cv2
+import cv2 as cv
 import math
 
-img = cv2.imread('Fig0316(3)(third_from_top).tif')
+img = cv.imread('Fig0304(a)(breast_digital_Xray).tif')
+imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+imgGray = (imgGray/255.0)
+print(imgGray)
+
+#Negativa
+imgNeg = 1.0 - imgGray
+
+#Transformacoes logaritmicas
+c = 500 / np.log(1 + np.max(imgGray)) 
+img_log = c*(np.log(1 + imgGray))
+img_log = np.array(img_log, dtype=np.uint8)
+
+#Correcao de gama
+img_gam = np.array(255*(imgGray) ** 5, dtype='uint8')
+
+cv.imshow('Imagem original', img)
+#cv.imshow('Imagem Negativa', imgNeg)
+#cv.imshow('Imagem Log', img_log)
+cv.imshow('Imagem Gamma', img_gam)
+cv.waitKey(0)
+cv.destroyAllWindows()
